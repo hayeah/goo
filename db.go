1aa211b6c2b2f7285130b04d8ffd77bfe2305acc
Howard Yeh <hayeah@gmail.com>
2024-04-28 15:13:45 +0800

    add db migration

--- db.go
+++ db.go
@@ -1,8 +1,12 @@
 package goo
 
 import (
+	"embed"
 	"fmt"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/jmoiron/sqlx"
 	"github.com/rs/zerolog"
 )
@@ -10,6 +14,8 @@ import (
 type DatabaseConfig struct {
 	Dialect string
 	DSN     string
+
+	MigrationsPath string
 }
 
 func ProvideSQLX(goocfg *Config, down *ShutdownContext, log *zerolog.Logger) (*sqlx.DB, error) {
@@ -30,4 +36,66 @@ func ProvideSQLX(goocfg *Config, down *ShutdownContext, log *zerolog.Logger) (*s
 	})
 
 	return db, err
+}
+
+// https://github.com/golang-migrate/migrate/blob/master/GETTING_STARTED.md
+// https://github.com/golang-migrate/migrate/blob/master/MIGRATIONS.md
+
+// ProvideMigrate provides a filesystem backed db migration.
+func ProvideMigrate(basecfg *Config) (*migrate.Migrate, error) {
+	if basecfg.Database == nil {
+		return nil, fmt.Errorf("no database configuration")
+	}
+
+	cfg := basecfg.Database
+
+	if cfg.MigrationsPath == "" {
+		return nil, fmt.Errorf("no MigrationsPath configured")
+	}
+
+	databaseURL := fmt.Sprintf("%s://%s", cfg.Dialect, cfg.DSN)
+	fileURL := fmt.Sprintf("file://%s", cfg.MigrationsPath)
+
+	m, err := migrate.New(fileURL, databaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+/*
+The benefit of embedding is to make it easier to distribute the application as a
+single binary. The embedded files are stored in the binary itself, so there is
+no need to distribute the migration files separately.
+
+// go:embed testdata/migrations/*.sql
+//  var fs embed.FS
+
+*/
+
+type EmbbededMigrate migrate.Migrate
+
+type EmbeddedMigrateConfig struct {
+	fs        embed.FS
+	embedPath string
+}
+
+// ProvideEmbbededMigrate provides an embed.FS based db migration.
+func ProvideEmbbededMigrate(embedCfg *EmbeddedMigrateConfig, cfg *Config) (*EmbbededMigrate, error) {
+	if cfg.Database == nil {
+		return nil, fmt.Errorf("no database configuration")
+	}
+
+	fs, err := iofs.New(embedCfg.fs, embedCfg.embedPath)
+	if err != nil {
+		return nil, err
+	}
+
+	m, err := migrate.NewWithSourceInstance("iofs", fs, cfg.Database.DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	return (*EmbbededMigrate)(m), err
 }
--- di.go
+++ di.go
@@ -2,4 +2,10 @@ package goo
 
 import "github.com/google/wire"
 
-var Wires = wire.NewSet(ProvideShutdownContext, ProvideZeroLogger, ProvideEcho, ProvideSQLX)
+var Wires = wire.NewSet(
+	ProvideShutdownContext,
+	ProvideZeroLogger,
+	ProvideEcho,
+	ProvideSQLX,
+	ProvideMigrate,
+)
