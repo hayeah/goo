fc2fe996de92c61462ba153d042af9e8d11ae3d1
Howard Yeh <hayeah@gmail.com>
2024-06-06 15:43:31 +0800

    rename fetch

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -1,79 +1,30 @@
 package fetch
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
+	"net/http"
+	"net/url"
 
-	"github.com/go-resty/resty/v2"
-	"github.com/hayeah/goo/sse"
 	"github.com/tidwall/gjson"
 )
 
-type HTTPError struct {
-	StatusCode int
-	Status     string
-}
-
-func (e *HTTPError) Error() string {
-	return fmt.Sprintf("http error: %s", e.Status)
-}
-
-func New(c *resty.Client) Client {
-	return Client{c}
-}
-
-type Client struct {
-	*resty.Client
-}
-
-// R returns a GJSONRequest
-func (c *Client) R() *Request {
-	return &Request{c.Client.R()}
-}
-
-func (c *Client) JSON(method, url string, opts *Options) (*Response, error) {
-	return c.R().JSON(method, url, opts)
-}
-
-type SSEResponse struct {
-	*Response
-	*sse.Scanner
-}
-
-func (r *SSEResponse) Close() error {
-	return r.Scanner.Close()
-}
-
-func (c *Client) SSE(method string, url string, opts *Options) (*SSEResponse, error) {
-
-	var opts2 Options
-	if opts != nil {
-		opts2 = *opts
-	}
-
-	opts2.RawResponseBody = true
-
-	res, err := c.R().JSON(method, url, &opts2)
-	if err != nil {
-		return nil, err
-	}
-
-	scanner := sse.NewScanner(res.RawBody(), false)
-
-	return &SSEResponse{res, scanner}, nil
-}
+// URLParams map[string]string
 
 type Options struct {
-	Method string
+	BaseURL    string
+	PathParams any
 
-	Body       any
+	Method     string
+	Header     http.Header
+	Body       any // []byte | string
 	BodyParams any
 
-	RawResponseBody bool
-
-	PathParams any // map[string]string
+	Client  *http.Client
+	Context context.Context
 }
 
 // Body returns the body of the request. If the body is a template, it will be rendered.
@@ -100,17 +51,60 @@ func (o *Options) RenderBody() ([]byte, error) {
 	return nil, nil
 }
 
-type Request struct {
-	*resty.Request
-}
-
-func (r *Request) JSON(method, url string, opts *Options) (*Response, error) {
-	if opts == nil {
-		opts = &Options{}
+func (o *Options) SetHeader(key, value string) {
+	if o.Header == nil {
+		o.Header = http.Header{}
 	}
 
-	if opts.RawResponseBody {
-		r.SetDoNotParseResponse(true)
+	o.Header.Set(key, value)
+}
+
+// Do creates a new request and executes it.
+func (o *Options) Do(resource string) (*http.Response, error) {
+	req, err := NewRequest(resource, o)
+	if err != nil {
+		return nil, err
+	}
+
+	var client *http.Client
+	if o.Client != nil {
+		client = o.Client
+	} else {
+		client = http.DefaultClient
+	}
+
+	return client.Do(req)
+}
+
+func NewRequest(resource string, opts *Options) (*http.Request, error) {
+	var err error
+
+	if opts.PathParams != nil {
+		resource, err = RenderURLPath(resource, opts.PathParams)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if opts.BaseURL != "" {
+		resource, err = url.JoinPath(opts.BaseURL, resource)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var ctx context.Context
+	if opts.Context != nil {
+		ctx = opts.Context
+	} else {
+		ctx = context.Background()
+	}
+
+	var method string
+	if opts.Method != "" {
+		method = opts.Method
+	} else {
+		method = http.MethodGet
 	}
 
 	body, err := opts.RenderBody()
@@ -118,83 +112,39 @@ func (r *Request) JSON(method, url string, opts *Options) (*Response, error) {
 		return nil, err
 	}
 
-	if opts.PathParams != nil {
-		switch opts.PathParams.(type) {
-		case map[string]string:
-			r.SetPathParams(opts.PathParams.(map[string]string))
-		default:
-			// TODO: use mapstructure to convert to map[string]string
-			// github.com/mitchellh/mapstructure
-			return nil, errors.New("path params should be map[string]string")
-		}
-
-	}
-
-	r.SetHeader("Content-Type", "application/json")
+	var bodyReader io.Reader
 	if body != nil {
-		r.SetBody(body)
+		bodyReader = io.NopCloser(bytes.NewReader(body))
 	}
 
-	// method := "GET"
-	// if opts.Method != "" {
-	// 	method = opts.Method
-	// }
-
-	res, err := r.Execute(method, url)
+	req, err := http.NewRequestWithContext(ctx, method, resource, bodyReader)
 	if err != nil {
 		return nil, err
 	}
 
-	// return http status error
-	// if res.IsError() {
-	// 	err = &HTTPError{
-	// 		StatusCode: res.StatusCode(),
-	// 		Status:     res.Status(),
-	// 	}
-	// }
-
-	return &Response{res}, err
+	req.Header = opts.Header
+	return req, nil
 }
 
-type Response struct {
-	*resty.Response
+type JSONResponse struct {
+	*http.Response
+
+	body []byte
 }
 
-func (r *Response) JSON() []byte {
-	// TODO: check json content type
-	return r.Body()
+// JSON decodes the JSON response from the server.
+func (r *JSONResponse) Decode(v interface{}) error {
+	return json.Unmarshal(r.body, v)
 }
 
-func (r *Response) String() string {
-	//
-	body := r.Body()
-	if body == nil {
-		//
-		data, err := io.ReadAll(r.RawBody())
-
-		if err != nil {
-			r.Request.SetError(err)
-		}
-		// r.SetBody(data)
-
-		// r.Request.SetError(errors.New("body is nil")
-		return string(data)
-	}
-
-	return string(r.Body())
+// Body returns the body of the response.
+func (r *JSONResponse) Body() []byte {
+	return r.body
 }
 
-func (r *Response) Close() error {
-	if r != nil {
-		return r.Response.RawBody().Close()
-	}
-
-	return nil
-}
-
-// GJSON queries body
-func (r *Response) GJSON(path string) GJSONResult {
-	return GJSONResult{gjson.GetBytes(r.Body(), path)}
+// Get queries json path using GJSON
+func (r *JSONResponse) Get(path string) GJSONResult {
+	return GJSONResult{gjson.GetBytes(r.body, path)}
 }
 
 type GJSONResult struct {
@@ -203,4 +153,31 @@ type GJSONResult struct {
 
 func (r GJSONResult) Unmarshal(o any) error {
 	return json.Unmarshal([]byte(r.Raw), o)
+}
+
+// String returns the body of the response as a string.
+func (r *JSONResponse) String() string {
+	return string(r.body)
+}
+
+func JSON(resource string, opts *Options) (*JSONResponse, error) {
+	// set content type to json is not set
+	// if opts.Header.Get("Content-Type") == "" {
+	// 	opts.SetHeader("Content-Type", "application/json")
+	// }
+
+	res, err := opts.Do(resource)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &JSONResponse{
+		Response: res,
+		body:     body,
+	}, nil
 }
--- fetch/fetch_test.go
+++ fetch/fetch_test.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/hayeah/goo/fetch/v2"
+	"github.com/hayeah/goo/fetch"
 )
 
 func TestRenderBody(t *testing.T) {
